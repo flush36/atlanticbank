51cb649a2697058a9fa1fa90a4bf27f181e63ce8
Thiago Ferreira <thiago.empreendedor01@gmail.com>
2018-09-12 20:10:29 -0300

    initial commit

---
+++ src/main/java/br/com/bancoatlantico/atlanticbank/AtlanticbankApplication.java
@@ -0,0 +1,12 @@
+package br.com.bancoatlantico.atlanticbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlanticbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlanticbankApplication.class, args);
+	}
+}
