aa5d80f2f96d3b85025f192c24b04c3e03f50783
Unknown <thiago.empreendedor01@gmail.com>
2018-09-14 18:49:31 -0300

    fix: (login) : funcionalidade de logar do usuario
    Esta sendo gerado um token de acesso ao usuario entrar na aplicação.

--- bin/main/db/migration/V02__criar_tabela_conta.sql
+++ bin/main/db/migration/V02__criar_tabela_conta.sql
@@ -1,8 +1,8 @@
-CREATE TABLE  (
+CREATE TABLE conta (
     idconta int not null auto_increment,
-    saldo decimal(10,2) not null,
-    saque decimal(10,2) not null,
-    deposito decimal(10,2) not null,
+    saldo decimal(10,2),
+    saque decimal(10,2),
+    deposito decimal(10,2),
     data_operacao datetime not null,
     fkuser int not null,
     constraint fk_user foreign key(fkuser)
